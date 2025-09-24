e90d5a2ea05912fd516ff88fb5d8419ecfdb9975
saivenkat-verifai <saivenkat.verifai@gmail.com>
2025-09-23 21:11:07 -0400

    version 18-19

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -25,16 +25,7 @@ interface DashboardCard {
     selector: "app-dashboard",
     templateUrl: "./dashboard.component.html",
     styleUrls: ["./dashboard.component.css"],
-    imports: [
-        CommonModule,
-        // TODO: `HttpClientModule` should not be imported into a component directly.
-        // Please refactor the code to add `provideHttpClient()` call to the provider list in the
-        // application bootstrap logic and remove the `HttpClientModule` import from this component.
-        CardModule,
-        ColumnChartComponent,
-        CalendarComponent,
-        LineChartComponent,
-    ]
+
 })
 export class DashboardComponent implements OnInit {
   currentDate = new Date();
